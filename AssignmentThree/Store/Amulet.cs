823a080608ab004a885f8022797de582e4ad3de2
Dennis Lindquist <dennislindquist@gmail.com>
2021-01-29 10:16:30 +0100

    adding shop

---
+++ AssignmentThree/Store/Amulet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentThree.Store
+{
+    class Amulet : Item
+    {
+        private int strength;
+        private int toughness;
+
+        public int Strength { get => strength; set => strength = value; }
+        public int Toughness { get => toughness; set => toughness = value; }
+    }
+}
